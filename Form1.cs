2e35d5d062e519668872e59963d0e77b1f713788
Sven H <sven.hansen@bbcag.ch>
2015-07-03 11:01:05 +0200

    dictionary

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,8 @@ namespace Memory
 
         public string path = "D:\\Users\\ehanss\\Documents\\Visual Studio 2013\\Projects\\Memory\\Memory\\Resources";
 
+        public Dictionary <Object,string> Feld = new Dictionary <Object,string>();
+
 
         public frm_Memory()
         {
@@ -33,9 +35,9 @@ namespace Memory
         private void picBox_XX_onClick(object sender, EventArgs e)
         {
             PictureBox picBox = (PictureBox)sender;
-            //MessageBox.Show(picBox.Image.n);
+            //MessageBox.Show(picBox.Image);
 
-            ProcessDirectory(path);
+            //ProcessDirectory(path);
         
 
 
@@ -56,38 +58,62 @@ namespace Memory
 
             int i = 0;
 
-            List<PictureBox>  picBoxes = new List<PictureBox>();
-
 
             
-            picBoxes.Add(picBox_00);
-            picBoxes.Add(picBox_01);
-            picBoxes.Add(picBox_02);
-            picBoxes.Add(picBox_03);
 
-            picBoxes.Add(picBox_10);
-            picBoxes.Add(picBox_11);
-            picBoxes.Add(picBox_12);
-            picBoxes.Add(picBox_13);
+            Feld.Add(picBox_00, picBox_00.ImageLocation);
+            Feld.Add(picBox_01, picBox_01.ImageLocation);
+            Feld.Add(picBox_02, picBox_02.ImageLocation);
+            Feld.Add(picBox_03, picBox_03.ImageLocation);
 
-            picBoxes.Add(picBox_20);
-            picBoxes.Add(picBox_21);
-            picBoxes.Add(picBox_22);
-            picBoxes.Add(picBox_23);
+            Feld.Add(picBox_10, picBox_10.ImageLocation);
+            Feld.Add(picBox_11, picBox_11.ImageLocation);
+            Feld.Add(picBox_12, picBox_12.ImageLocation);
+            Feld.Add(picBox_13, picBox_13.ImageLocation);
 
-            picBoxes.Add(picBox_30);
-            picBoxes.Add(picBox_31);
-            picBoxes.Add(picBox_32);
-            picBoxes.Add(picBox_33);
+            Feld.Add(picBox_20, picBox_20.ImageLocation);
+            Feld.Add(picBox_21, picBox_21.ImageLocation);
+            Feld.Add(picBox_22, picBox_22.ImageLocation);
+            Feld.Add(picBox_23, picBox_23.ImageLocation);
 
-            foreach (string fileName in fileEntries)
+            Feld.Add(picBox_30, picBox_30.ImageLocation);
+            Feld.Add(picBox_31, picBox_31.ImageLocation);
+            Feld.Add(picBox_32, picBox_32.ImageLocation);
+            Feld.Add(picBox_33, picBox_33.ImageLocation);
+
+
+            //list<picturebox> picboxes = new list<picturebox>();
+
+
+
+            //picboxes.add(picbox_00);
+            //picboxes.add(picbox_01);
+            //picboxes.add(picbox_02);
+            //picboxes.add(picbox_03);
+
+            //picboxes.add(picbox_10);
+            //picboxes.add(picbox_11);
+            //picboxes.add(picbox_12);
+            //picboxes.add(picbox_13);
+
+            //picboxes.add(picbox_20);
+            //picboxes.add(picbox_21);
+            //picboxes.add(picbox_22);
+            //picboxes.add(picbox_23);
+
+            //picboxes.add(picbox_30);
+            //picboxes.add(picbox_31);
+            //picboxes.add(picbox_32);
+            //picboxes.add(picbox_33);
+
+
+
+
+            foreach (KeyValuePair<Object, string> entry in Feld)
             {
-                if (i <= 15)
-                {
-                    MemoryHelp.ProcessFile((PictureBox)picBoxes[i], fileName);
-                    i++;
-                }
+                MemoryHelp.ProcessFile((PictureBox)entry.Key, entry.Value);
             }
+
 
             
 	
